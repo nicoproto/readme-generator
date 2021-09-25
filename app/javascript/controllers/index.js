8801b45cae907575df8636980bcffa80a207fc27
Nico Proto <nicolasproto@gmail.com>
2021-09-25 12:11:32 +0200

    FIX: Fixed Stimulus because of version update

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,10 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-const application = Application.start()
+window.Stimulus = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+// const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
