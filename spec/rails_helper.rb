525a4c241ad58e650edbd0edcf5d2b37c0806153
Nico Proto <nicolasproto@gmail.com>
2021-09-25 14:32:20 +0200

    FEAT: Added configuration for Shoulda Matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,3 +64,11 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 end
+
+# Shoulda Matchers configuration
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
